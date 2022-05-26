624e9b68e6bf109efa3fa23975f06ec4f4b5f40a
hitesh <hitesh22rana@gmail.com>
2022-05-26 15:23:15 +0530

    Dummy data and Context API is added

---
+++ src/dummyData/userFeed.js
@@ -0,0 +1,29 @@
+const UserFeed = [
+    {
+        'name' : 'Arjun Reddy',
+        'activeAgo' : '10 min ago',
+        'post' : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi, dicta illum laudantium debitis tenetur ex praesentium velit porro et beatae laborum similique cum, unde accusantium fugit architecto iste quisquam?'
+    },
+    {
+        'name' : 'Arjun Reddy',
+        'activeAgo' : '10 min ago',
+        'post' : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi, dicta illum laudantium debitis tenetur ex praesentium velit porro et beatae laborum similique cum, unde accusantium fugit architecto iste quisquam?'
+    },
+    {
+        'name' : 'Arjun Reddy',
+        'activeAgo' : '10 min ago',
+        'post' : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi, dicta illum laudantium debitis tenetur ex praesentium velit porro et beatae laborum similique cum, unde accusantium fugit architecto iste quisquam?'
+    },
+    {
+        'name' : 'Arjun Reddy',
+        'activeAgo' : '10 min ago',
+        'post' : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi, dicta illum laudantium debitis tenetur ex praesentium velit porro et beatae laborum similique cum, unde accusantium fugit architecto iste quisquam?'
+    },
+    {
+        'name' : 'Arjun Reddy',
+        'activeAgo' : '10 min ago',
+        'post' : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi, dicta illum laudantium debitis tenetur ex praesentium velit porro et beatae laborum similique cum, unde accusantium fugit architecto iste quisquam?'
+    },
+]
+
+export { UserFeed };
---
+++ src/dummyData/userFollower.js
@@ -0,0 +1,16 @@
+const UserFollowers = [
+    {
+        'name' : 'Arjun Reddy',
+        'following' : '200',
+    },
+    {
+        'name' : 'Arjun Reddy',
+        'following' : '200',
+    },
+    {
+        'name' : 'Arjun Reddy',
+        'following' : '200',
+    }
+]
+
+export { UserFollowers };
